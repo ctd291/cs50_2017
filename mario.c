7e5892012accf1f9eb5f43aaebacbb88c701ab3d
Thomas Brittain <honeysucklehardware@gmail.com>
2017-07-19 07:29:31 -0500

    Mario!

--- mario.c
+++ mario.c
@@ -1,6 +1,43 @@
 #include <cs50.h>
 #include <stdio.h> 
 
+void print_leading_blocks(int number_to_print, int number_of_spaces);
+void print_trailing_blocks(int number_to_print, int number_of_spaces);
+
 int main() {
-    printf("Howdy!\n");
+    
+    // Get number of lines from the user
+    int number_of_lines = 0;
+    do{
+        number_of_lines = get_int();
+        if(number_of_lines < 1 || number_of_lines > 23){
+            printf("No, stop it.  For real this time.\n");
+        }
+    }
+    while(number_of_lines < 1 || number_of_lines > 23);
+    
+    for(int height = 1; height < number_of_lines + 1; height++){
+        print_leading_blocks(height, number_of_lines - height);
+        printf(" ");
+        print_trailing_blocks(number_of_lines - height, height);
+        printf("\n");
+    }
+}
+
+void print_leading_blocks(int number_to_print, int number_of_spaces){
+    for(int i = 0; i < number_of_spaces; i++){
+        printf(" ");
+    }
+    for(int i = 0; i < number_to_print; i++){
+        printf("#");
+    }
+}
+
+void print_trailing_blocks(int number_to_print, int number_of_spaces){
+        for(int i = 0; i < number_of_spaces; i++){
+        printf("#");
+    }
+    for(int i = 0; i < number_to_print; i++){
+        printf(" ");
+    }
 }
